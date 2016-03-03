fc11e0898abe9ad8aa87e15abdfe7d506795d901
王超 <1985088337@qq.com>
2016-03-03 18:51:35 +0800

    添加httpmothed

--- OS.Http/Enum.cs
+++ OS.Http/Enum.cs
@@ -1,21 +1,35 @@
 ﻿
 namespace OS.Http
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public enum HttpMothed
     {
         /// <summary>
         /// 防止没有设置默认值，根据此值判断
         /// </summary>
         None,
+
         /// <summary>
-        /// Get 
+        /// Get
         /// </summary>
         GET = 2,
+
         /// <summary>
         /// post
         /// </summary>
-        POST = 4
+        POST = 4,
 
+        /// <summary>
+        /// PUT
+        /// </summary>
+        PUT=8,
+
+        /// <summary>
+        /// DELETE
+        /// </summary>
+        DELETE=16
     }
 
     /// <summary>
@@ -23,13 +37,33 @@ namespace OS.Http
     /// </summary>
     public enum ResponseStatus
     {
+        /// <summary>
+        /// 没有响应
+        /// </summary>
         None,
+        /// <summary>
+        /// 响应ok
+        /// </summary>
         Completed,
+        /// <summary>
+        /// 响应出错
+        /// </summary>
         Error,
+        /// <summary>
+        /// 响应出错但正确返回数据
+        /// </summary>
         ErrorButResponse,
+        /// <summary>
+        /// 超市
+        /// </summary>
         TimedOut,
+        /// <summary>
+        /// 取消
+        /// </summary>
         Aborted
     }
+
+
     /// <summary>
     /// 平台参数类型
     /// </summary>
