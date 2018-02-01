0912fc0f42fe78410e583320b66a864da80c5d26
KevinWG <1985088337@qq.com>
2018-02-01 20:23:40 +0800

    1. 更新名称错误HttpMothed=>HttpMethod 2. 升级基础类库

--- OSS.Http/Mos/OsHttpRequest.cs
+++ OSS.Http/Mos/OsHttpRequest.cs
@@ -41,7 +41,7 @@ namespace OSS.Http.Mos
         /// <summary>
         /// 请求方式
         /// </summary>
-        public HttpMothed HttpMothed { get; set; } = HttpMothed.GET;
+        public HttpMethod HttpMothed { get; set; } = HttpMethod.GET;
 
         ///// <summary>
         /////  是否允许自动重定向
