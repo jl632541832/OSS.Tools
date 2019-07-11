d7e315488baac56ca3a91c8bca632ad4ae2315f1
KevinWG <1985088337@qq.com>
2019-07-11 23:56:38 +0800

    添加日志等其他工具

---
+++ Config/OSS.Tools.DirConfig/DirConfigUtil.cs
@@ -0,0 +1,85 @@
+﻿#region Copyright (C) 2016 Kevin (OSS开源系列) 公众号：OSSCore
+
+/***************************************************************************
+*　　	文件功能描述：全局插件 -  配置插件辅助类
+*
+*　　	创建人： Kevin
+*       创建人Email：1985088337@qq.com
+*       
+*       
+*****************************************************************************/
+
+#endregion
+
+using System;
+using OSS.Common.ComModels;
+using OSS.Common.Plugs;
+
+namespace OSS.Tools.DirConfig
+{
+    /// <summary>
+    /// 字典配置通用存储获取信息
+    /// </summary>
+    [Obsolete("请使用 OSS.Tools.DirConfig 命名空间下 DirConfigUtil ")]
+    public static class DirConfigUtil
+    {
+        private static readonly DefaultDirConfigPlug defaultCache = new DefaultDirConfigPlug();
+
+        /// <summary>
+        ///   配置信息模块提供者
+        /// </summary>
+        public static Func<string, IDirConfigPlug> DirConfigProvider { get; set; }
+
+        /// <summary>
+        /// 通过模块名称获取
+        /// </summary>
+        /// <param name="dirConfigModule"></param>
+        /// <returns></returns>
+        private static IDirConfigPlug GetDirConfig(string dirConfigModule)
+        {
+            if (string.IsNullOrEmpty(dirConfigModule))
+                dirConfigModule = ModuleNames.Default;
+
+            return DirConfigProvider?.Invoke(dirConfigModule) ?? defaultCache;
+        }
+
+
+        /// <summary>
+        /// 设置字典配置信息
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="dirConfig"></param>
+        /// <param name="moduleName">模块名称</param>
+        /// <typeparam name="TConfig"></typeparam>
+        /// <returns></returns>
+        public static ResultMo SetDirConfig<TConfig>(string key, TConfig dirConfig,
+            string moduleName = ModuleNames.Default) where TConfig : class, new()
+        {
+            return GetDirConfig(moduleName).SetDirConfig(key, dirConfig);
+        }
+
+
+        /// <summary>
+        ///   获取字典配置
+        /// </summary>
+        /// <typeparam name="TConfig"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="moduleName">模块名称</param>
+        /// <returns></returns>
+        public static TConfig GetDirConfig<TConfig>(string key,  string moduleName = ModuleNames.Default) where TConfig : class ,new()
+        {
+            return GetDirConfig(moduleName).GetDirConfig<TConfig>(key);
+        }
+
+        /// <summary>
+        ///  移除配置信息
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="moduleName">模块名称</param>
+        /// <returns></returns>
+        public static ResultMo RemoveDirConfig( string key, string moduleName = ModuleNames.Default)
+        {
+            return GetDirConfig(moduleName).RemoveDirConfig(key);
+        }
+    }
+}
