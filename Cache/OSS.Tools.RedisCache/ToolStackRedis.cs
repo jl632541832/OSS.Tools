083124f5ae71e698ba9631a0127cc74dc6967fd5
osscore <1985088337@qq.com>
2019-12-21 22:38:53 +0800

    修改名称

--- Cache/OSS.Tools.RedisCache/ToolStackRedis.cs
+++ Cache/OSS.Tools.RedisCache/ToolStackRedis.cs
@@ -8,7 +8,7 @@ namespace OSS.Tools.RedisCache
     /// <summary>
     /// redis缓存实现类
     /// </summary>
-    public class StackRedisTool : IToolCache
+    public class ToolStackRedis : IToolCache
     {
         //redis数据库连接字符串
         private readonly string ConnectionStr = null;
@@ -24,7 +24,7 @@ namespace OSS.Tools.RedisCache
         /// </summary>
         /// <param name="db"></param>
         /// <param name="connectStr"></param>
-        public StackRedisTool(int db, string connectStr)
+        public ToolStackRedis(int db, string connectStr)
         {
             ConnectionStr = connectStr;
             this._db = db;
