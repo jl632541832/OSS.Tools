5ba1815d6aa2c3ed1a6c667a2b4f02e97bb1d2e3
kevin <1985088337@qq.com>
2021-04-23 10:59:57 +0800

    数据传输过程的堆栈缓冲处理类库

---
+++ OSS.Tools.DataStack/IDataSackProvider.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace OSS.Tools.DataStack
+{
+    public interface IDataSackProvider
+    {
+        IStackPusher<TData> CreateStack<TData>(IStackPoper<TData> poper, string sourceName = "default");
+    }
+}
